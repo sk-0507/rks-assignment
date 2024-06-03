5738b27aeff98378ff33a3fb0ba05c8c4f074bdf
sk-0507 <111683663+sk-0507@users.noreply.github.com>
2024-06-03 15:46:22 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
  
 const url =
-  "mongodb+srv://suraj:suraj@cluster0.qpoaow1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
+  "mongodb+srv://<uname>:<password>@cluster0.qpoaow1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
 const Connection = async () => {
   await mongoose.connect(url).then(() => {
     console.log("Database is connected!");
